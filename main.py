5a9768d654fafa618bbf1d7538fdc64976bf3bd8
Kilian Koeltzsch <me@kilian.io>
2026-01-28 10:18:13 +0100

    bind against 0.0.0.0

--- main.py
+++ main.py
@@ -19,4 +19,4 @@ async def root():
     return RedirectResponse(url="/docs")
 
 if __name__ == "__main__":
-    uvicorn.run("main:app", host="192.168.178.45", port=8000, reload=True)
+    uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=True)
