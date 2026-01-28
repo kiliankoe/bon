161cadc45e43e3a4ff3f1edb801a8f98919399ed
Kilian Koeltzsch <me@kilian.io>
2026-01-28 18:28:09 +0100

    override incorrect printer profile data with actual printable width

--- printer.py
+++ printer.py
@@ -1,5 +1,5 @@
 from escpos.printer import Usb
 
 # TODO: Consider reading VendorID and ProductID from a configuration file or environment variables
-# TODO: Figure out how to set a valid media width pixel field for centering media
 printer = Usb(0x04b8, 0x0e28, profile="TM-T88III")
+printer.profile.profile_data["media"]["width"]["pixels"] = 576
